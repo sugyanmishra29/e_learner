e6bba075ad04dacc3f9f2657dbbe726b97c137bf
khiladimahan1210 <gupta18e@iitg.ac.in>
2019-10-19 20:48:06 +0530

    1

--- lib/options.dart
+++ lib/options.dart
@@ -1,4 +1,4 @@
-import 'package:e_learner/books.dart';
+import 'books.dart';
 import 'package:flutter/material.dart';
 import 'videos.dart';
 
@@ -30,7 +30,7 @@ class OptionScreenState extends State<OptionScreen> {
           body: TabBarView(
             children: [
               videoSearch(),
-              BooksSearch(),
+              bookSearch(),
               Icon(Icons.directions_bike),
             ],
           ),
