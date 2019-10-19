d43a9f4693c6db65b4b5a7ef8fe780126b78b26c
Yash <yash.kmr.99@gmail.com>
2019-10-19 18:54:20 +0530

    btwn

--- lib/videos.dart
+++ lib/videos.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+final dbRef = FirebaseDatabase.instance.reference();
 
 class videoSearch extends StatefulWidget {
 
@@ -8,10 +11,36 @@ class videoSearch extends StatefulWidget {
 }
 
 class videoSearchState extends State<videoSearch> {
+
+String res = "videos";
+
+myFunc() {
+var data= dbRef.child("Books");
+res += "videos";
+data.once().then((DataSnapshot snapshot) {
+  var values = snapshot.value;
+  print (values["Mathematics"]);
+  var keys = [];
+  for (var k in values) keys.add(k);
+  setState(() {
+    res += "videos";
+    for (var k in keys) res += k;
+  });
+});
+}
   Widget build(BuildContext) {
     return Column(
       children: <Widget>[
-        Text("Videos"),
+        Text(res),
+        RaisedButton(
+          animationDuration: Duration(milliseconds: 300),
+          color: Colors.blue,
+          padding: const EdgeInsets.all(15),
+          child: Text('Reload',
+              style: TextStyle(fontSize: 15, color: Colors.white)),
+          onPressed: () { myFunc();
+          },
+        ),
       ],
     );
   }
