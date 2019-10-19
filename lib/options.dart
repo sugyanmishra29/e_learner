0e5a0a56e9f346335b7ddbf41c833e42299d1661
Yash <yash.kmr.99@gmail.com>
2019-10-19 14:53:58 +0530

    added homescreen

---
+++ lib/options.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class OptionScreen extends StatefulWidget {
+  @override
+  OptionScreenState createState() {
+    return new OptionScreenState();
+  }
+}
+
+class OptionScreenState extends State<OptionScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+            title: Text('Tabs Demo'),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
