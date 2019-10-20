f7d8e873003f1362934cc7590a90a263735315e7
khiladimahan1210 <gupta18e@iitg.ac.in>
2019-10-20 23:01:03 +0530

    gradient setup in options.dart

--- lib/options.dart
+++ lib/options.dart
@@ -33,6 +33,19 @@ class OptionScreenState extends State<OptionScreen> {
               ],
             ),
             title: Text('E_Learner IITG'),
+            flexibleSpace: Container(
+              decoration: new BoxDecoration(
+                gradient: new LinearGradient(
+                    colors: [
+                      const Color(0xFF3366FF),
+                      const Color(0xFF00CCFF),
+                    ],
+                    begin: const FractionalOffset(0.0, 0.0),
+                    end: const FractionalOffset(1.0, 0.0),
+                    stops: [0.0, 1.0],
+                    tileMode: TileMode.clamp),
+              ),
+            ),
             actions: <Widget>[
               new FlatButton(
                 child: new Text('Logout',
